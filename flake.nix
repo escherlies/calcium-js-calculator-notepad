139048f473096130704f580707bc4ef1b388feea
Enrico Scherlies <e.scherlies@pm.me>
2025-09-19 12:40:13 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    with inputs;
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+      nixosConfigurations = { };
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          common = {
+            buildInputs = with pkgs; [
+              miniserve
+
+            ];
+
+            env = {
+              # Env vars passed to nix shell
+            };
+          };
+        in
+        {
+          default = pkgs.mkShell (
+            common
+            // {
+
+              shellHook = ''
+                echo "Hello, nix shell"
+
+                miniserve --index index.html .
+              '';
+            }
+          );
+        }
+      );
+
+    };
+}
